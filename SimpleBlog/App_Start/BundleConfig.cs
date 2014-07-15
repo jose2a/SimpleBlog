b4fa1d7436b18a0f9cc1d7534b38ac6ddd8e0339
Jose A <josearriag@hotmail.com>
2014-07-15 15:19:45 -0400

    Imported styles and scriots (jQuery and Bootstrap)

---
+++ SimpleBlog/App_Start/BundleConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace SimpleBlog
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/admin/styles")
+                .Include("~/Content/Styles/bootstrap.css")
+                .Include("~/Content/Styles/Admin.css"));
+
+            bundles.Add(new StyleBundle("~/styles")
+                .Include("~/Content/Styles/bootstrap.css")
+                .Include("~/Content/Styles/Site.css"));
+
+            bundles.Add(new ScriptBundle("~/admin/scripts")
+                .Include("~/Scripts/jquery-2.1.1.js")
+                .Include("~/Scripts/jquery.validate.js")
+                .Include("~/Scripts/jquery.validate.unobtrusive.js")
+                .Include("~/Scripts/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/scripts")
+                .Include("~/Scripts/jquery-2.1.1.js")
+                .Include("~/Scripts/jquery.validate.js")
+                .Include("~/Scripts/jquery.validate.unobtrusive.js")
+                .Include("~/Scripts/bootstrap.js"));
+        }        
+    }
+}
